1e5055dac90e6faf7a300271adbc7c21532da80a
elpapuuXD <eltomascenturion@gmail.com>
2025-03-31 03:34:14 -0300

    Add task management routes with CRUD operations

---
+++ src/routes/taskRoutes.ts
@@ -0,0 +1,42 @@
+import { Router, Request, Response } from "express";
+import { AppDataSource } from "../data-source";
+import { Task } from "../entities/Task";
+
+const router = Router();
+const taskRepository = AppDataSource.getRepository(Task);
+
+router.get("/tasks", async (__dirname, res) => {
+    const tasks = await taskRepository.find()
+    res.json(tasks)
+})
+
+router.post("/tasks", async (req, res) => {
+    const {title} = req.body;
+    const newTask = taskRepository.create({title})
+    await taskRepository.save(newTask)
+    res.status(201).json(newTask)
+})
+
+router.put("/tasks/:id", async (req, res ): Promise<any> =>{
+    const { id } = req.params;
+    const {status} = req.body
+    const task = await taskRepository.findOneBy({id: parseInt(id)})
+
+    if(!task) return res.status(404).json({message: "Task not found"})
+
+    task.status = status
+    await taskRepository.save(task)
+    return res.json(task)
+})
+
+router.delete("/tasks/:id", async (req, res): Promise<any> => {
+    const {id} = req.params;
+    const task = await taskRepository.findOneBy({id: parseInt(id)})
+
+    if (!task) return res.status(404).json({message: "Task not found"})
+
+    await taskRepository.remove(task)
+    res.json({message: "Task deleted"})
+})
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import { AppDataSource } from "./data-source";
+import taskRoutes from "./routes/taskRoutes";
 
-const app = express()
+
+const app = express();
 app.use(express.json());
+app.use(taskRoutes);
 
 const PORT = 3000;
+
+AppDataSource.initialize()
+.then(() => {
+console.log("Database connected successfully");
+
 app.listen(PORT, () =>{
     console.log(`Server is running on http://localhost:${PORT}`);
+});
 })
+.catch((error) => console.log(error));
